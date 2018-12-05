92bcd397c6e283ab848b2959ad0cb4a4066cf177
Hakim Ghods <ghods.hakim@hotmail.com>
2018-12-05 21:47:18 +0330

    Add project files.

---
+++ NetApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var height = int.Parse(textBox1.Text);
+            var width = int.Parse(textBox2.Text);
+
+            label3.Text = (height * width).ToString();
+        }
+    }
+}
